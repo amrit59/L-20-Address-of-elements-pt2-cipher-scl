c52787d23bad2ef68c2c1da2c22362cfa91c1a3d
Amrit Raj <121791502+amrit59@users.noreply.github.com>
2024-07-18 14:46:09 +0530

    Add files via upload

---
+++ address of elements 2.cpp
@@ -0,0 +1,27 @@
+/*#include<iostream>
+using namespace std;
+int main(){
+	cout<<"hello India"<<endl;
+	cout<<"jai ho";
+	return 0;	
+}*/
+#include<iostream>
+using namespace std;
+int main(){
+	int a , b ,c;
+	  a=15;
+	  b=16;
+	  c=51;
+	  cout<<"a*b"
+
+}
+/*
+#include<iostream>
+using namespace std;
+int main(){
+	long long int a;
+	
+	int b;
+	cout<<"The size of int is :"<<sizeof(b)<<endl<<"The size of long long int is: "<<sizeof(a);
+	return 0;
+}*/
